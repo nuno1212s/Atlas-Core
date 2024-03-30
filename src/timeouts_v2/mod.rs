3648688f261ebadbda1b22a7b73edfa2df99abfe
Nuno Neto <nuno.neto.g@gmail.com>
2024-03-30 20:20:01 +0000

    Almost done with new timeouts module, working on some unit tests.

--- src/timeouts_v2/mod.rs
+++ src/timeouts_v2/mod.rs
@@ -1,15 +1,23 @@
-mod worker;
-
-use std::any::Any;
+use dyn_clone::DynClone;
 use std::cmp::Ordering;
+use std::collections::BTreeMap;
+use std::fmt::Debug;
 use std::hash::Hash;
 use std::sync::Arc;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
+
 use getset::{CopyGetters, Getters};
-use atlas_common::channel::ChannelSyncTx;
+use itertools::Itertools;
+
+use crate::request_pre_processing::operation_key_raw;
+use atlas_common::channel::{new_bounded_sync, ChannelSyncTx};
 use atlas_common::node_id::NodeId;
 use atlas_common::ordering::SeqNo;
+
 use crate::timeouts_v2::worker::WorkerMessage;
+
+mod tests;
+mod worker;
 
 #[derive(Hash, Eq, PartialEq, Clone, Debug)]
 pub enum TimeoutID {
@@ -17,6 +25,7 @@ pub enum TimeoutID {
     SessionBased {
         session: SeqNo,
         seq_no: SeqNo,
+        from: NodeId,
     },
 }
 
@@ -24,6 +33,19 @@ pub enum TimeoutID {
 pub struct TimeoutIdentification {
     mod_id: Arc<str>,
     timeout_id: TimeoutID,
+}
+
+/// A time-out that has occurred
+#[derive(Debug, CopyGetters, Getters)]
+pub struct Timeout {
+    #[get = "pub"]
+    id: TimeoutIdentification,
+    #[get_copy = "pub"]
+    timeout_count: usize,
+    #[get_copy = "pub"]
+    timeout_time: SystemTime,
+    #[get = "pub"]
+    extra_info: Option<Box<dyn TimeOutable>>,
 }
 
 #[derive(CopyGetters, Getters)]
@@ -36,18 +58,11 @@ struct TimeoutRequest {
     needed_acks: usize,
     #[get_copy]
     is_cumulative: bool,
-    #[get]
-    extra_info: Option<Box<dyn Any>>,
+    extra_info: Option<Box<dyn TimeOutable>>,
 }
 
 /// A timeout trait, representing the behaviour needed from a timeout request
-pub trait TimeOut {
-    // The identification of the timeout
-    fn id(&self) -> &TimeoutIdentification;
-
-    // How many confirmed responses do we need to extinguish the timeout?
-    fn needed_acks(&self) -> usize;
-}
+pub trait TimeOutable: DynClone + Debug + Send {}
 
 #[derive(Getters, CopyGetters)]
 struct TimeoutAck {
@@ -62,6 +77,117 @@ pub struct TimeoutsHandle {
     worker_handles: Vec<ChannelSyncTx<WorkerMessage>>,
 }
 
+pub fn initialize_timeouts<WR>(
+    our_id: NodeId,
+    num_workers: usize,
+    channel_size: usize,
+    timeout_output: WR,
+) -> TimeoutsHandle
+where
+    WR: TimeoutWorkerResponder + 'static,
+{
+    let mut handles = Vec::with_capacity(num_workers);
+
+    for worker in 0..num_workers {
+        let (tx, rx) = new_bounded_sync(
+            channel_size,
+            Some(format!("TimeoutWorker Thread {}", worker)),
+        );
+
+        worker::initialize_worker_thread(our_id, worker, rx, timeout_output.clone());
+
+        handles.push(tx)
+    }
+
+    TimeoutsHandle {
+        worker_handles: handles,
+    }
+}
+
+impl TimeoutsHandle {
+    fn worker_for_timeout(
+        &self,
+        timeout_id: &TimeoutIdentification,
+    ) -> &ChannelSyncTx<WorkerMessage> {
+        &self.worker_handles[self.worker_id_for_timeout(timeout_id)]
+    }
+
+    fn worker_id_for_timeout(&self, timeout_id: &TimeoutIdentification) -> usize {
+        match timeout_id.timeout_id {
+            TimeoutID::SeqNoBased(_) => 0,
+            TimeoutID::SessionBased { session, from, .. } => {
+                operation_key_raw(from, session) as usize % self.worker_handles.len()
+            }
+        }
+    }
+
+    pub fn request_timeout(
+        &self,
+        timeout_id: TimeoutIdentification,
+        extra_info: Option<Box<dyn TimeOutable>>,
+        duration: Duration,
+        needed_acks: usize,
+        cumulative: bool,
+    ) -> atlas_common::error::Result<()> {
+        self.worker_for_timeout(&timeout_id)
+            .send(WorkerMessage::Request(TimeoutRequest {
+                id: timeout_id,
+                duration,
+                needed_acks,
+                is_cumulative: cumulative,
+                extra_info,
+            }))
+    }
+
+    pub fn request_timeouts(
+        &self,
+        timeout_id: Vec<(TimeoutIdentification, Option<Box<dyn TimeOutable>>)>,
+        duration: Duration,
+        needed_acks: usize,
+        cumulative: bool,
+    ) -> atlas_common::error::Result<()> {
+        timeout_id
+            .into_iter()
+            .map(|(id, extra_info)| TimeoutRequest {
+                id,
+                duration,
+                needed_acks,
+                is_cumulative: cumulative,
+                extra_info,
+            })
+            .group_by(|rq| self.worker_id_for_timeout(rq.id()))
+            .into_iter()
+            .try_for_each(|(worker_id, group)| {
+                self.worker_handles[worker_id].send(WorkerMessage::Requests(group.collect()))
+            })
+    }
+
+    pub fn ack_received(
+        &self,
+        timeout_id: TimeoutIdentification,
+        from: NodeId,
+    ) -> atlas_common::error::Result<()> {
+        self.worker_for_timeout(&timeout_id)
+            .send(WorkerMessage::Ack(TimeoutAck {
+                id: timeout_id,
+                from,
+            }))
+    }
+
+    pub fn acks_received(
+        &self,
+        acks: Vec<(TimeoutIdentification, NodeId)>,
+    ) -> atlas_common::error::Result<()> {
+        acks.into_iter()
+            .map(|(id, from)| TimeoutAck { id, from })
+            .group_by(|ack| self.worker_id_for_timeout(ack.id()))
+            .into_iter()
+            .try_for_each(|(worker_id, group)| {
+                self.worker_handles[worker_id].send(WorkerMessage::Acks(group.collect()))
+            })
+    }
+}
+
 impl PartialOrd for TimeoutID {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
@@ -72,15 +198,38 @@ impl Ord for TimeoutID {
     fn cmp(&self, other: &Self) -> Ordering {
         match (self, other) {
             (TimeoutID::SeqNoBased(a), TimeoutID::SeqNoBased(b)) => a.cmp(b),
-            (TimeoutID::SessionBased { session: session_1, seq_no: seq_1 },
-                TimeoutID::SessionBased { session: session_2, seq_no: seq_2 }) => {
-                match session_1.cmp(session_2) {
+            (
+                TimeoutID::SessionBased {
+                    session: session_1,
+                    seq_no: seq_1,
+                    from: from_1,
+                },
+                TimeoutID::SessionBased {
+                    session: session_2,
+                    seq_no: seq_2,
+                    from: from_2,
+                },
+            ) => match from_1.cmp(from_2) {
+                Ordering::Equal => match session_1.cmp(session_2) {
                     Ordering::Equal => seq_1.cmp(seq_2),
                     other => other,
-                }
-            }
+                },
+                other => other,
+            },
             (TimeoutID::SeqNoBased(_), TimeoutID::SessionBased { .. }) => Ordering::Less,
             (TimeoutID::SessionBased { .. }, TimeoutID::SeqNoBased(_)) => Ordering::Greater,
         }
+    }
+}
+
+/// The trait detailing the worker behaviour we require
+pub trait TimeoutWorkerResponder: Send + Clone {
+    /// Report a timeout to the rest of the system
+    fn report_timeouts(&self, timeouts: Vec<Timeout>) -> atlas_common::error::Result<()>;
+}
+
+impl TimeoutRequest {
+    fn extra_info(&self) -> Option<&dyn TimeOutable> {
+        self.extra_info.as_deref()
     }
 }
--- src/timeouts_v2/worker/mod.rs
+++ src/timeouts_v2/worker/mod.rs
@@ -1,8 +1,10 @@
+use getset::{CopyGetters, Getters};
+use log::error;
 use std::cell::RefCell;
 use std::collections::{BTreeMap, BTreeSet};
+use std::ops::Add;
 use std::rc::Rc;
 use std::time::{Duration, SystemTime, SystemTimeError};
-use getset::{CopyGetters, Getters};
 
 use thiserror::Error;
 
@@ -10,13 +12,15 @@ use atlas_common::channel::ChannelSyncRx;
 use atlas_common::collections::HashMap;
 use atlas_common::node_id::NodeId;
 
-use crate::timeouts_v2::{TimeOut, TimeoutAck, TimeoutIdentification, TimeoutRequest};
+use crate::timeouts_v2::{
+    Timeout, TimeoutAck, TimeoutIdentification, TimeoutRequest, TimeoutWorkerResponder,
+};
 
 pub(super) enum WorkerMessage {
-    TimeoutRequest(TimeoutRequest),
-    TimeoutRequests(Vec<TimeoutRequest>),
-    TimeoutAck(TimeoutAck),
-    TimeoutAcks(Vec<TimeoutAck>),
+    Request(TimeoutRequest),
+    Requests(Vec<TimeoutRequest>),
+    Ack(TimeoutAck),
+    Acks(Vec<TimeoutAck>),
 }
 
 enum TimeoutPhase {
@@ -39,7 +43,7 @@ struct RegisteredTimeout {
     request: TimeoutRequest,
 }
 
-struct TimeoutWorker {
+struct TimeoutWorker<WR> {
     our_node_id: NodeId,
     worker_id: u32,
     default_timeout_duration: Duration,
@@ -52,43 +56,121 @@ struct TimeoutWorker {
 
     // The timeouts we are currently waiting for
     pending_timeout_heap: BTreeMap<u64, Vec<Rc<RefCell<RegisteredTimeout>>>>,
+    // The notifier to send timeouts to
+    timeout_notifier: WR,
 }
 
-impl TimeoutWorker {
-    fn process_message(&mut self, message: WorkerMessage) -> Result<(), ProcessTimeoutMessageError> {
+pub(super) fn initialize_worker_thread<WR>(
+    our_id: NodeId,
+    worker_id: usize,
+    work_receiver: ChannelSyncRx<WorkerMessage>,
+    notifier: WR,
+) where
+    WR: TimeoutWorkerResponder + 'static,
+{
+    std::thread::Builder::new()
+        .name(format!("TimeoutWorker-{}", worker_id))
+        .spawn(move || {
+            let mut worker = TimeoutWorker {
+                our_node_id: our_id,
+                worker_id: worker_id as u32,
+                default_timeout_duration: Default::default(),
+                work_rx_channel: work_receiver,
+                watched_requests: Default::default(),
+                pending_timeout_heap: Default::default(),
+                timeout_notifier: notifier,
+            };
+            
+            loop {
+                if let Err(err) = worker.run() {
+                    error!("Timeout worker error: {:?}", err);
+                }
+            }
+        })
+        .expect("Failed to spawn timeout worker thread");
+}
+
+impl<WR> TimeoutWorker<WR>
+where
+    WR: TimeoutWorkerResponder,
+{
+    fn run(&mut self) -> Result<(), TimeoutError> {
+        let duration = Duration::from_millis(1);
+
+        loop {
+            match self.work_rx_channel.recv_timeout(duration) {
+                Ok(message) => {
+                   self.process_message(message)?;
+                }
+                Err(e) => {
+                    error!("Error receiving message: {:?}", e);
+                }
+            }
+
+            self.process_timeouts()?;
+        }
+    }
+    
+    fn process_message(
+        &mut self,
+        message: WorkerMessage,
+    ) -> Result<(), ProcessTimeoutMessageError> {
         match message {
-            WorkerMessage::TimeoutRequest(request) => {
+            WorkerMessage::Request(request) => {
                 self.handle_timeout_request(request)?;
             }
-            WorkerMessage::TimeoutRequests(requests) => {
-                requests.into_iter().try_for_each(|rq| {
-                    self.handle_timeout_request(rq)
-                })?;
+            WorkerMessage::Requests(requests) => {
+                requests
+                    .into_iter()
+                    .try_for_each(|rq| self.handle_timeout_request(rq))?;
             }
-            WorkerMessage::TimeoutAck(ack) => {
+            WorkerMessage::Ack(ack) => {
                 self.handle_timeout_ack(ack)?;
             }
-            WorkerMessage::TimeoutAcks(acks) => {
-                acks.into_iter().try_for_each(|ack| {
-                    self.handle_timeout_ack(ack)
-                })?;
+            WorkerMessage::Acks(acks) => {
+                acks.into_iter()
+                    .try_for_each(|ack| self.handle_timeout_ack(ack))?;
             }
         }
 
         Ok(())
     }
 
-    fn handle_timeout_request(&mut self, timeout_request: TimeoutRequest) -> Result<(), ProcessTimeoutError> {
+    fn handle_timeout_request(
+        &mut self,
+        timeout_request: TimeoutRequest,
+    ) -> Result<(), ProcessTimeoutError> {
         let registered_rq = Rc::new(RefCell::new(RegisteredTimeout::new(timeout_request)?));
 
-        self.watched_requests.entry(registered_rq.borrow().request().id().clone())
+        self.watched_requests
+            .entry(registered_rq.borrow().request().id().clone())
             .or_insert(registered_rq.clone());
 
         let timeout_time = registered_rq.borrow().timeout_time();
 
-        self.pending_timeout_heap.entry(timeout_time)
+        self.pending_timeout_heap
+            .entry(timeout_time)
             .or_default()
             .push(registered_rq);
+
+        Ok(())
+    }
+
+    fn re_register_timeout(
+        &mut self,
+        timeout: Rc<RefCell<RegisteredTimeout>>,
+    ) -> Result<(), ProcessTimeoutError> {
+        let timeout_time = SystemTime::now()
+            .add(*timeout.borrow().request().duration())
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis() as u64;
+
+        timeout.borrow_mut().timeout_time = timeout_time;
+
+        self.pending_timeout_heap
+            .entry(timeout_time)
+            .or_default()
+            .push(timeout);
 
         Ok(())
     }
@@ -120,37 +202,94 @@ impl TimeoutWorker {
             }
         }
     }
-    
+
     fn process_timeouts(&mut self) -> Result<(), TimeoutError> {
+        let current_sys_time = SystemTime::now();
+
+        let now = current_sys_time
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis() as u64;
+
+        let mut timeouts = Vec::new();
         
-        let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_millis() as u64;
-        
-        while let Some((timeout, _))  =self.pending_timeout_heap.first_key_value() {
+        println!("Timeouts: {:?}", self.pending_timeout_heap.keys());
+        println!("Now: {:?}", now);
+
+        while let Some((timeout, _)) = self.pending_timeout_heap.first_key_value() {
             if *timeout > now {
                 break;
             }
-            
+
             let (_, requests) = self.pending_timeout_heap.pop_first().unwrap();
-            
-            requests.iter()
-                .map(|rq| rq.borrow_mut())
-                .for_each(|mut rq| rq.timed_out());
-            
-            
+
+            requests
+                .iter()
+                .for_each(|mut rq| rq.borrow_mut().timed_out());
+
+            let (cumulative_stream, non_cumulative_stream): (Vec<_>, Vec<_>) = requests
+                .iter()
+                .partition(|rq| rq.borrow().request().is_cumulative());
+
+            // Re register the cumulative timeouts for the next timeout
+            cumulative_stream
+                .into_iter()
+                .try_for_each(|rq| self.re_register_timeout(rq.clone()))?;
+
+            // Remove the non-cumulative ones from the watched list
+            non_cumulative_stream.into_iter().for_each(|rq| {
+                self.watched_requests.remove(rq.borrow().request().id());
+            });
+
+            let mut partial_timeouts = requests
+                .into_iter()
+                .map(|rq| {
+                    let rq_borrow = rq.borrow();
+
+                    let extra_info = rq_borrow
+                        .request()
+                        .extra_info
+                        .as_ref()
+                        .map(|ex| dyn_clone::clone_box(&**ex));
+
+                    Timeout {
+                        id: rq_borrow.request().id().clone(),
+                        timeout_count: rq_borrow.timeout_phase().timeout_count(),
+                        timeout_time: current_sys_time,
+                        extra_info,
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            timeouts.append(&mut partial_timeouts);
         }
-        
+
+        if !timeouts.is_empty() {
+            self.timeout_notifier.report_timeouts(timeouts)?;
+        }
+
         Ok(())
     }
-    
 }
 
 impl TimeoutPhase {
     fn next_timeout(&self) -> Self {
         match self {
             Self::NeverTimedOut => Self::TimedOut(1, SystemTime::now()),
-            Self::TimedOut(n, _) => {
-                Self::TimedOut(*n + 1, SystemTime::now())
-            }
+            Self::TimedOut(n, _) => Self::TimedOut(*n + 1, SystemTime::now()),
+        }
+    }
+
+    fn timeout_count(&self) -> usize {
+        match self {
+            Self::NeverTimedOut => 0,
+            Self::TimedOut(n, _) => *n,
+        }
+    }
+
+    fn last_timeout_time(&self) -> Option<SystemTime> {
+        match self {
+            Self::NeverTimedOut => None,
+            Self::TimedOut(_, t) => Some(*t),
         }
     }
 }
@@ -159,7 +298,9 @@ impl RegisteredTimeout {
     fn new(request: TimeoutRequest) -> Result<Self, SystemTimeError> {
         let end_timeout_time = SystemTime::now() + request.duration;
 
-        let timeout_int_time = end_timeout_time.duration_since(SystemTime::UNIX_EPOCH)?.as_millis() as u64;
+        let timeout_int_time = end_timeout_time
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis() as u64;
 
         Ok(Self {
             timeout_time: timeout_int_time,
@@ -177,8 +318,8 @@ impl RegisteredTimeout {
 
         Ok(())
     }
-    
-    fn timed_out(&mut self)  {
+
+    fn timed_out(&mut self) {
         self.timeout_phase = self.timeout_phase.next_timeout();
     }
 }
@@ -194,18 +335,23 @@ pub enum ProcessTimeoutMessageError {
 #[derive(Error, Debug)]
 pub enum AcceptAckError {
     #[error("Node already ACKed {0:?}")]
-    NodeAlreadyAcked(NodeId)
+    NodeAlreadyAcked(NodeId),
 }
 
 #[derive(Error, Debug)]
 pub enum ProcessTimeoutError {
     #[error("Failed to calculate system time? {0}")]
-    SystemTimeError(#[from] SystemTimeError)
+    SystemTimeError(#[from] SystemTimeError),
 }
 
 #[derive(Error, Debug)]
 pub enum TimeoutError {
     #[error("Failed to calculate system time? {0}")]
-    SystemTimeError(#[from] SystemTimeError),
-    
+    SystemTime(#[from] SystemTimeError),
+    #[error("Re-Register cumulative timeout error {0}")]
+    RegisterTimeout(#[from] ProcessTimeoutError),
+    #[error("Failed to process message timeouts {0}")]
+    MessageProcess(#[from] ProcessTimeoutMessageError),
+    #[error("Failed to notify of timeouts {0}")]
+    Notifier(#[from] anyhow::Error),
 }
